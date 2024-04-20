5b238b288cf3d1e6a9c191fc41b4898c260ebb15
Bushcowboy <87616296+Bushcowboy@users.noreply.github.com>
2024-04-19 20:56:21 -0400

    Update client.lua
    Did some changes suggested by C-FOUR switched to use ox

--- bush-blackmarket/client/client.lua
+++ bush-blackmarket/client/client.lua
@@ -1,30 +1,25 @@
-Citizen.CreateThread(function() -- Changed CreateThread to Citizen.CreateThread
-    local model = GetHashKey("a_m_m_indian_01") -- Changed `a_m_m_indian_01` to GetHashKey("a_m_m_indian_01")
-    RequestModel(model)
-    while not HasModelLoaded(model) do
-        Wait(0)
-    end
-    local entity = CreatePed(0, model, Config.Ped.x, Config.Ped.y, Config.Ped.z, Config.Ped.w, false) -- Changed Config.Ped to Config.Ped.x, etc.
-    
-    -- Freeze the ped in place
-    FreezeEntityPosition(entity, true)
-    
-    -- Make the ped invincible
-    SetEntityInvincible(entity, true)
+CreateThread(function()
+    for _, v in pairs(Config.Peds) do
+        local modelHash = v.hash
+        local coords = v.location
+        lib.requestModel(modelHash, 500)
+        local spawnedPed = CreatePed(0, modelHash, coords.x, coords.y, coords.z-1.0, coords.w, false, false)
+        TaskStartScenarioInPlace(spawnedPed, 'WORLD_HUMAN_CLIPBOARD', -1)
+        FreezeEntityPosition(spawnedPed, true)
+        SetEntityInvincible(spawnedPed, true)
+        SetBlockingOfNonTemporaryEvents(spawnedPed, true)
 
-    exports['qb-target']:AddTargetEntity(entity, {
-        options = {
+        exports.ox_target:addLocalEntity(spawnedPed, {
             {
-                type = "client",
-                icon = 'fas fa-crab',
-                label = 'Attachments Menu',
-                action = function()
+                icon = 'fas fa-circle',
+                label = 'Open BlackMarket',
+                onSelect = function()
                     OpenMenu()
-                end
-            }
-        },
-        distance = 2.5,
-    })
+                end,
+                distance = 2.0
+            },
+        })
+    end
 end)
 
 function OpenMenu()
@@ -34,18 +29,20 @@ function OpenMenu()
         icon = 'gun',
         options = {},
     }
-
+    
     for _, shopType in ipairs(Config.ShopTypes) do
-        print("Inserting menu item for shop type:", shopType.label)
-        table.insert(menuItems.options, {
+        local newItem = {
             title = 'Open Shop',
             description = shopType.label .. ' Shop',
             icon = 'fas fa-code-merge',
             onSelect = function()
                 TriggerEvent('qb-weaponshops:client:OpenShop', shopType.type)
             end,
-        })
+        }
+        menuItems.options[#menuItems.options + 1] = newItem
     end
+    
+    
 
     lib.registerContext(menuItems)
     lib.showContext('blackmarket')
@@ -54,5 +51,4 @@ end
 RegisterNetEvent('qb-weaponshops:client:OpenShop')
 AddEventHandler('qb-weaponshops:client:OpenShop', function(shopType)
     exports.ox_inventory:openInventory('shop', { type = shopType, id = 1 })
-    print("Opened inventory for shop type:", shopType)
 end)
